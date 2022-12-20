1e378782377aa8cbd1dffc067d6ed23ddfdd6e64
vjlion <89218741998@mail.ru>
2022-12-20 20:37:55 +0300

    seminar

---
+++ seminar1/Program.cs
@@ -0,0 +1,24 @@
+﻿/*12 Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+
+34, 5 -> не кратно, остаток 4 
+16, 4 -> кратно
+
+*/
+
+void  Ostatok(int numberA, int numberB)
+{
+    int ost = numberA%numberB;
+    if (ost==0)
+    {
+        Console.WriteLine("Числа кратны");
+    }
+    else
+    {
+       Console.WriteLine($"Остаток  {ost}"); 
+    }
+}
+Console.Write("Введите первое число ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+int number2 = int.Parse(Console.ReadLine());
+Ostatok(number1,number2);
---
+++ seminar2/Program.cs
@@ -0,0 +1,25 @@
+﻿/*14 Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 
+7 и 23.
+
+14 -> нет 
+46 -> нет 
+161 -> да
+*/
+void Ostatok(int numberA)
+{
+    int ost1 = numberA % 7;
+    int ost2 = numberA % 23;
+    
+    if (ost1 == 0 && ost2 == 0)
+    {
+        Console.WriteLine("Число одновременно кратно 7 и 23");
+    }
+    else
+    {
+        Console.WriteLine("Число одновременно не кратно 7 и 23"); 
+    }
+}
+
+Console.Write("Введите число ");
+int number = int.Parse(Console.ReadLine());
+Ostatok(number);
