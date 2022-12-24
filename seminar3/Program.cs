847df8af80fed6d4eb0ad28e25259d8c27e57c15
vjlion <89218741998@mail.ru>
2022-12-24 11:50:44 +0300

    задача16

--- seminar3/Program.cs
+++ seminar3/Program.cs
@@ -5,3 +5,23 @@
 25, 5  ->  да
 8,9  ->  нет
 */
+Console.WriteLine("введите число 1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число 2");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int quadro1 = Quadro(num1);
+int quadro2 = Quadro(num2);
+
+if (quadro1 == num2 || quadro2 == num1)
+{
+Console.Write("Да");
+}
+else 
+{
+  Console.Write("Нет");  
+}
+
+int Quadro (int a)
+{
+    return a * a;
+}
